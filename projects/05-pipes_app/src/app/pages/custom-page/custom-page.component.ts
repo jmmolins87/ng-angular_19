005e690d522b25e4de65083cbf693b8b0826f388
Juanma Molins Cortés <35576283+jmmolins87@users.noreply.github.com>
2025-03-12 19:00:14 +0100

    finsh section pipes

--- projects/05-pipes_app/src/app/pages/custom-page/custom-page.component.ts
+++ projects/05-pipes_app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+
+
+import { Component, signal } from '@angular/core';
+
+import { heroes } from '../../data/heros.data';
+
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroColorPipe } from '../../pipes/heroColor.pipe';
+import { HeroCreatorPipe } from "../../pipes/hero-creator.pipe";
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { HeroTextColorPipe } from "../../pipes/hero-text-color.pipe";
+import { TitleCasePipe } from '@angular/common';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [
+    CanFlyPipe,
+    HeroColorPipe,
+    HeroCreatorPipe,
+    HeroFilterPipe,
+    HeroSortByPipe,
+    HeroTextColorPipe,
+    TitleCasePipe,
+    ToggleCasePipe,
+],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+
+  name = signal<string>('Juanma Molins')
+  upper = signal<boolean>(true);
+  heroes = signal<Hero[]>(heroes);
+  sortBy = signal<keyof Hero | null>(null);
+  searchQuery = signal<string>('');
+}
---
+++ projects/05-pipes_app/src/app/pipes/heroColor.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, type PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'heroColor',
+})
+export class HeroColorPipe implements PipeTransform {
+
+  transform(value: number): string {
+    switch (value) {
+      case 0:
+        return 'red';
+      break;
+      case 1:
+        return 'black';
+      break;
+      case 2:
+        return 'blue'
+      break;
+      case 3:
+        return 'green'
+      break;
+      default:
+        return 'No color';
+      break;
+    }
+  }
+}
