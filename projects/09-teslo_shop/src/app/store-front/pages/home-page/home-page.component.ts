59ae3661d8389ff12d0ec24b8b5258cac33711bc
Juanma <jmmolins87@gmail.com>
2025-03-20 18:44:00 +0100

    add backend teslo project and wip

---
+++ projects/09-teslo_shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+import { Component } from '@angular/core';
+
+import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
+
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ProductCardComponent],
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent { }
