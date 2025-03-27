bb0fb5ee9141a6ec78abf3f069e49d6106865770
Juanma <jmmolins87@gmail.com>
2025-03-27 13:36:13 +0100

    delete front teslo shop

--- projects/09-teslo_shop/src/app/products/components/product-card/product-card.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'product-card',
-  imports: [],
-  templateUrl: './product-card.component.html',
-})
-export class ProductCardComponent { }
--- projects/09-teslo_shop/src/app/store-front/pages/product-page/product-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-product-page',
-  imports: [],
-  templateUrl: './product-page.component.html',
-})
-export class ProductPageComponent { }
